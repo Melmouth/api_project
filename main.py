6934129a37a4cd9c1bd9b90f9c33699f7c1bd5ca
Melvine <melvine.leclerc@gmail.com>
2025-02-21 14:30:23 +0100

    V1

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+
+app = FastAPI()
+
+class InputPayload(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+class OutputPayload(BaseModel):
+    prediction: str
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello World, this is a reloaded test"}
+
+model = joblib.load_model('model.pkl')
+
+@app.post("/echo")
+async def echo(payload: InputPayload):
+    return payload
+
+@app.post("/predict", response_model=OutputPayload)
+def send_back_information(payload: InputPayload):
+    input = input.dict()
+    prediction = model.predict(input['sepal_length'],
+                                input['sepal_width'],
+                                input['petal_length'],
+                                input['petal_width'])
+    map_predict_to_label = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
+    return {'prediction': map_predict_to_label[prediction]}
